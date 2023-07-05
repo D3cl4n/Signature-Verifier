e58acab03b6b8eb88771947e3485ca5aeced0af0
D3cl4n <dpmurphy@wpi..edu>
2023-07-05 10:11:00 -0400

    Add project files.

---
+++ Signature-Verifier/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Signature_Verifier;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SignatureVerifier handler = new SignatureVerifier();
+        handler.VerifySignature(args);
+    }
+}
