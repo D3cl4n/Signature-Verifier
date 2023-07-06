2edae2c921da9d535d09cc41b22f81117191f9b5
D3cl4n <dpmurphy@wpi..edu>
2023-07-06 11:40:11 -0400

    more work

--- Signature-Verifier/SignatureVerification.cs
+++ Signature-Verifier/SignatureVerification.cs
@@ -13,7 +13,12 @@ namespace Signature_Verifier
         public MetadataValues metadata;
         public string targetFile;
 
-        private byte[]? computeExpectedHash()
+        private string byteArrToHex(byte[] arr)
+        {
+            return BitConverter.ToString(arr);
+        }
+
+        private byte[]? computeActualHash()
         {
             byte[]? hash = null;
             string hashAlg = String.Empty;
@@ -31,11 +36,32 @@ namespace Signature_Verifier
         }
         public void verifySignature()
         {
-            byte[] certificateData = Convert.FromBase64String(metadata.certificate);
+            string pemCertificate = "-----BEGIN CERTIFICATE-----\n" + metadata.certificate + "\n-----END CERTIFICATE-----";
+            byte[] certificateData = Encoding.ASCII.GetBytes(pemCertificate);
             X509Certificate2 cert = new X509Certificate2(certificateData);
             RSA publicKey = cert.GetRSAPublicKey();
-            byte[]? expectedHash = computeExpectedHash();
-            Console.WriteLine($"Expected hash of xlsm: {expectedHash.ToString}");
+            if (publicKey == null)
+            {
+                Console.WriteLine("Null pub key");
+            }
+            byte[]? actualHash = computeActualHash();
+            string hexHash = byteArrToHex(actualHash).Replace("-", "");
+            Console.WriteLine($"Actual hash of xlsm: {hexHash}");
+            string pemSignature = "-----BEGIN SIGNATURE-----\n" + metadata.signature + "\n-----END SIGNATURE-----";
+            byte[]? digitalSignature = Encoding.ASCII.GetBytes(pemSignature);
+            byte[] actualData = File.ReadAllBytes(targetFile);
+
+            bool isValid = publicKey.VerifyData(actualData, digitalSignature, HashAlgorithmName.SHA256, RSASignaturePadding.Pss);
+
+            if (isValid)
+            {
+                Console.WriteLine("Signature is valid!");
+            }
+
+            else
+            {
+                Console.WriteLine("Signature is not valid");
+            }
         }
 
         public SignatureVerification(MetadataValues metadata, string filePath) 
